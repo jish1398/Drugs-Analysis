4ac2f3e15edafe446eb20d5db11128011a8ad182
jish1398 <jishnunair1398@gmail.com>
2018-10-16 22:14:59 +0530

    Changes

--- Sample.py
+++ Sample.py
@@ -5,16 +5,10 @@ Created on Tue Oct 16 21:57:17 2018
 @author: jishn
 """
 
-#import pandas as pd
+import pandas as pd
 
-#data=pd.read_csv('C:/Users/jishn/Desktop/DP/DATA/drugsComTest_raw.tsv',delimiter='\t',encoding='utf-8')
-#print(data.head)
+# Reading the file 
+data=pd.read_csv('C:/Users/jishn/Desktop/DP/DATA/drugsComTest_raw.tsv',delimiter='\t',encoding='utf-8')
+print(data.head)
 
 
-import csv
-
-with open('C:/Users/jishn/Desktop/DP/DATA/drugsComTest_raw.tsv') as tsvfile:
-  reader = csv.reader(tsvfile, delimiter='\t')
-  for row in reader:
-    print(row)
-    
